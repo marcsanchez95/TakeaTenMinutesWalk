6581b6d6e020fda3d6d53c56ae87606246d3a830
Marc <marcsmoreno@hotmail.com>
2025-04-10 12:19:58 +0200

    kata uokiad

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean result = TenMinWalk.isValid( new char [] {'n','s','s','s','s','s','n','n','n','n'});
+        System.out.println(result);
+    }
+    public static class TenMinWalk {
+        public static boolean isValid(char[] walk) {
+            if(walk.length != 10) {
+                return false;
+            }
+            boolean walkIsValid = false;
+           int x = 0;
+           int y = 0;
+            for(char direction : walk){
+                if(direction == 'n'){
+                    y++;
+                }
+                if(direction == 's'){
+                    y--;
+                }
+                if(direction == 'e'){
+                    x++;
+                }
+                if(direction == 'w'){
+                    x--;
+                }
+            }
+            if (x == 0 && y == 0) {
+                walkIsValid = true;
+            }
+            return walkIsValid;
+        }
+    }
+}
